b3577f855f480b65c18716ce0191af1f912e51e0
David Wikler <dwikler@Davids-MacBook-Pro.local>
2021-08-03 00:38:59 -0400

    Iteration 2

--- validate.js
+++ validate.js
@@ -1,16 +1,22 @@
 module.exports = function validate(str) {
   const out = {};
+  console.log(`==============================================================`);
+  console.log(`Testing password: ${str}`);
+  console.log(`==============================================================`);
 
   if (str.length < 8) {
     out.minLengthError = true;
+    console.log(`Password must be at least 8 characters long`);
   }
 
   if (!/\d/.test(str)) {
     out.numbersRequiredError = true;
+    console.log(`Password must contain at least 1 number`);
   }
 
   if (!/[a-zA-Z]/.test(str)) {
     out.lettersRequiredError = true;
+    console.log(`Password must contain at least 1 letter`);
   }
 
   return out;
